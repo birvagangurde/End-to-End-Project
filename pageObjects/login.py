1580b0ba758a7de43373a8156fc3f8e90a182434
birvagangurde <gangurdebirva@gmail.com>
2025-05-03 20:32:52 +0530

    Adding path manually in the file for fixing the no module found error

--- pageObjects/login.py
+++ pageObjects/login.py
@@ -1,6 +1,9 @@
+import os
+import sys
+
 from selenium.webdriver.common.by import By
 
-
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
