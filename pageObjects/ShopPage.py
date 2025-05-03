f680063825f7e8176f979e6d853b422342fe100a
birvagangurde <gangurdebirva@gmail.com>
2025-05-03 21:22:28 +0530

    Finalised shoppage code file till go to the cart

--- pageObjects/ShopPage.py
+++ pageObjects/ShopPage.py
@@ -8,6 +8,8 @@ class ShopPage:
         self.driver = driver
         self.shop_link = (By.CSS_SELECTOR, "a[href*='shop']")
         self.product_cards = (By.XPATH, '//div[@class="card h-100"]')
+        self.checkout_button = (By.CSS_SELECTOR, "a[class*='btn-primary']")
+
 
     def add_product_to_cart(self, product_name):
         self.driver.find_element(*self.shop_link).click()
@@ -17,4 +19,7 @@ class ShopPage:
             product_Name = product.find_element(By.XPATH, "div/h4/a").text
             if product_Name == product_name:
                 product.find_element(By.XPATH, "div/button").click()
+
+    def goToCart(self):
+        self.driver.find_element(*self.checkout_button).click()
 
--- test_e2eTestFramework.py
+++ test_e2eTestFramework.py
@@ -17,13 +17,7 @@ def test_e2e(browserInstance):
 
 
     # //a[contains(@href, 'shop')]         a[href*='shop']    #two ways of using regular expression
-    driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
-    products = driver.find_elements(By.XPATH, '//div[@class="card h-100"]')
 
-    for product in products:
-        product_Name = product.find_element(By.XPATH, "div/h4/a").text
-        if product_Name == "Blackberry":
-            product.find_element(By.XPATH, "div/button").click()
 
     driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
 
